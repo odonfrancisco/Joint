0609e3b1b37b31466b59e40c73c7e9cba8b9b0b0
fodon11 <francisco.odon11@gmail.com>
2018-08-22 00:03:28 -0400

    Added route to view particular menu. A little nervous about the login/signup session not being stored but should be sorted out by tomorrow

--- server/routes/menu.js
+++ server/routes/menu.js
@@ -4,9 +4,19 @@ const Menu = require('../models/Menu');
 const MenuItem = require('../models/MenuItem');
 
 router.get('/', (req, res, next) => {
-    Menu.find().populate('subMenus.items')
+    Menu.find()
         .then(menus => {
             res.status(200).json(menus)
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+})
+
+router.get('/:id', (req, res, next) => {
+    Menu.findById(req.params.id).populate('subMenus.items')
+        .then(menu => {
+            res.status(200).json(menu)
         })
         .catch(err => {
             res.status(500).json(err)
